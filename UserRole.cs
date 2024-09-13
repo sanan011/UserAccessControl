4d7df71e5dbcca50384b91540519ba4176834dc6
Sanan Yagubov <159418654+sanan011@users.noreply.github.com>
2024-09-13 12:23:01 +0400

    Create UserRole.cs

---
+++ UserRole.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace User_login_page
+{
+    internal enum UserRole
+    {
+        admin,
+        user
+    }
+}
